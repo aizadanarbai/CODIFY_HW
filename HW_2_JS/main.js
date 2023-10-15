2ed7594dd121d54e2bcf4e6899390170c2c66ad6
aizadanarbai <aizadaanarbai1410@gmail.com>
2023-10-15 21:03:59 +0600

    HW_2_JS

---
+++ HW_2_JS/main.js
@@ -0,0 +1,140 @@
+const age = 'Несовершеннолетний'
+console.log(age)
+
+const isStudent = true
+const Student = 23
+console.log(Student == isStudent)
+
+const num1 = 12
+const num2 = 50
+console.log(12 + 50)
+
+const day = 1
+
+switch (1) {
+    case 1:
+        console.log("1 это Понидельник.")
+        console.log("2 это Вторник")
+        console.log("3 это Среда")
+        console.log("4 это четверг")
+        console.log("5 это Пятница" )
+        console.log( "6 это Субота")
+        console.log("7 это Воскресение")
+        break;
+}
+
+const temperature = 30
+const cold = 15
+const hot = 80
+const warm = 30
+
+if (temperature === cold) {
+    console.log ('YES')
+} else if(temperature === 80 ) {
+    console.log ('temperature, 80')
+} else {
+    console.log('NO')
+}
+
+const score = 100
+const a = 100
+const  b = 20
+const midle = 60
+
+if (score === a ) {
+    console.log("Сдал")
+} else if (score === 60 ){
+    console.log (score, 60)
+} else {
+    console.log("Не сдал")
+}
+
+const num = 5555
+if (num === 5555) {
+    console.log ("Четное")
+} else{
+    console.log("Не четное")
+} 
+
+
+const language = "JavaScript"
+const languageit = "Pyton"
+
+if (language === "JavaScript" ) {
+    console.log ("Поддерживается")
+} else {
+    console.log ("Не поддерживается")
+}
+
+const c = 85
+const d = 50
+const e = 20
+console.log(c > d)
+console.log(d < e)
+
+const f = "fruit"
+const fruit = "Яблоко"
+const banan = "Желтый"
+const orange = "Оранжевый"
+console.log ("Яблоко")
+
+const grade = "Семь"
+
+switch ("Семь"){
+    case "Семь": {
+        console.log("A - Шесть")
+        console.log("B - Пять ")
+        console.log("C - Четыри ")
+        console.log("D - Три")
+
+    }
+}
+
+const x = 88
+const y = 99
+console.log (x < y)
+console.log (x > y)
+console.log (x == y)
+
+const isRaining = true
+if (isRaining == true ) {
+    console.log ("Едет дождь")
+} else {
+    console.log("Дождя нет")
+}
+
+
+const isLeapYear = true
+if (isLeapYear == true ) {
+    console.log ("Год високосный")
+} else {
+    console.log("Год не високосный")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+    
