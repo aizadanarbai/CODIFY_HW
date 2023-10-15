29e7aa838d64e33b831442e497c5513d7c2272fc
aizadanarbai <aizadaanarbai1410@gmail.com>
2023-10-15 14:29:28 +0600

    HW_1_JS

---
+++ HW_1_JS/main.js
@@ -0,0 +1,26 @@
+console.log ("Name")
+
+const Name = "Aizada"
+console.log ("Aizada");
+
+let Age = 23
+console.log (23)
+
+let City = "Batken" 
+console.log("Batken")
+    
+
+const num = 14
+console.log (num)
+
+const text = 'зеленный'
+console.log(text)
+
+const obj = {
+    Number: 14,
+    color: 'зеленный',
+}
+console.log(obj)
+
+
+
